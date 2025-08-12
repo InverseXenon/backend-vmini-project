3954fe29e053e26410c2677c0450f236afaf11bb
Piyush Patil <piyushpatil1741@gmail.com>
2025-08-12 17:57:36 +0530

    Final Update of the project.

--- server.js
+++ server.js
@@ -1,13 +1,55 @@
 const express = require('express')
 const app = express()
 const PORT = 8383
+
+let data = {
+    users: ['Piyush']
+}
+
+// Middleware
+app.use(express.json())
+
+// Website Endpoints
 app.get('/',(req,res) => {
-    console.log('Yay I hit an endpoint',req.method)
-    res.sendStatus(200)
+    res.send(`
+        <body style="background:pink; color: blue;">
+            <h1>Data:</h1>
+                <p>${JSON.stringify(data)}</p>
+                <a href ="/dashboard">Dashboard</a>
+        </body>
+        `)
 })
 app.get('/dashboard',(req,res) => {
-    console.log('Yay I hit an /dashboard endpoint')
-    res.send("HI")
+    console.log('User requested the homepage.')
+    res.send(`
+        <body> 
+        <a href ="/">Homepage</a>
+        <h1>Dashboard</h1>
+        </body> 
+        <script> console.log('This is my Script')</script>   
+        `)
+})
+
+
+// API Endpoints
+app.get('/api/data', (req,res) =>{
+    console.log(`I am accessing the data.`)
+    res.send(data)
+})
+
+app.post('/api/data', (req,res) =>{
+    // someone wants to create a user(for example they want to click a sign up button.)
+
+    const newEntry = req.body
+    console.log(newEntry)
+    data.users.push(newEntry.name)
+    res.sendStatus(201)
+})
+
+app.delete('/api/data', (req,res) => {
+    data.users.pop()
+    console.log('We deleted the last element of the array')
+    res.sendStatus(203)
 })
 
 app.listen(PORT, () => {
